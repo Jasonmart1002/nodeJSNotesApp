45b52a15e49866c550d76929b847e78f47c6bd22
Jasonmart1002 <Jason.mart1002@gmail.com>
2021-08-18 18:58:53 -0400

    notes firt test

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-const add = require('./utils.js')
+const getNotes = require('./notes.js')
 
-const sum = add(4, -2)
+const msg = getNotes()
 
-console.log(sum)
+console.log(msg)
+
+
+
+
+
+
+// const add = require('./utils.js')
+
+// const sum = add(4, -2)
+
+// console.log(sum)
