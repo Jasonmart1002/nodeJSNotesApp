610f6c56d5be830d0037f5a1c2723d5371395e45
Jasonmart1002 <Jason.mart1002@gmail.com>
2021-08-18 18:30:09 -0400

    appendFileSync

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const fs = require('fs')
 
-fs.writeFileSync('notes.txt','My name is Jason.')
+//fs.writeFileSync('notes.txt','My name is Jason.')
+
+fs.appendFileSync('notes.txt', 'I am from miami.')
