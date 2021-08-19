a659406a48cb7663e3544281be22606b5914cbed
Jasonmart1002 <Jason.mart1002@gmail.com>
2021-08-19 19:50:37 -0400

    DD

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const chalk = require('chalk');
+const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
-//customize yargs version
+// Customize yargs version
 yargs.version('1.1.0')
 
-//create add comand
+// Create add command
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
@@ -21,13 +21,12 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function(argv) {
-        console.log('Title: ' + argv.title)
-        console.log('Body: ' + argv.body)
+    handler: function (argv) {
+        notes.addNote(argv.title, argv.body)
     }
 })
 
-//create remove command
+// Create remove command
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
@@ -36,27 +35,22 @@ yargs.command({
     }
 })
 
-//create read command
-
-yargs.command({
-    command: 'read',
-    describe: 'Reading a note',
-    handler: function () {
-        console.log('Reading the note')
-    }
-})
-
-//create list command
-
+// Create list command
 yargs.command({
     command: 'list',
-    describe: 'Listing a Note',
+    describe: 'List your notes',
     handler: function () {
-        console.log('Listing the note')
+        console.log('Listing out all notes')
     }
 })
 
-//add, remove, read, list
-
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
 
 yargs.parse()
