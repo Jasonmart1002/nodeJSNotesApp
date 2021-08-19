a3053876b6f4a4ac95aa47ac48a951c00929922d
Jasonmart1002 <Jason.mart1002@gmail.com>
2021-08-19 13:37:11 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const chalk = require('chalk');
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
+const command = process.argv[2]
 
-const greenMsg = chalk.green.bold.inverse('Error!')
+console.log(process.argv)
 
-console.log(greenMsg);
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
