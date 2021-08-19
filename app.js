4d46a121378ad11be092e4b3c2e2c5e58193a6b1
Jasonmart1002 <Jason.mart1002@gmail.com>
2021-08-19 15:50:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,5 +2,9 @@ const chalk = require('chalk');
 const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-console.log(process.argv)
+//customize yargs version
+yargs.version('1.1.0')
+
+//add, remove, read, list
+
 console.log(yargs.argv)
