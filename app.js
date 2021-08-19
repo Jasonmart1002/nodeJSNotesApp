98ce5a70cc50beb085c7e460900e4fafc433d800
Jasonmart1002 <Jason.mart1002@gmail.com>
2021-08-19 16:02:20 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,44 @@ const getNotes = require('./notes.js')
 //customize yargs version
 yargs.version('1.1.0')
 
+//create add comand
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        console.log('Adding a new note!')
+    }
+})
+
+//create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+
+//create read command
+
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function () {
+        console.log('Reading the note')
+    }
+})
+
+//create list command
+
+yargs.command({
+    command: 'list',
+    describe: 'Listing a Note',
+    handler: function () {
+        console.log('Listing the note')
+    }
+})
+
 //add, remove, read, list
 
 console.log(yargs.argv)
